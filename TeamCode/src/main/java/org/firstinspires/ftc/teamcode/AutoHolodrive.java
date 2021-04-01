02719f9b09eedf6bbf6ae4d14542f2150f1f2f6a
Steve <github@conlanclynes.com>
2021-04-01 15:00:18 -0500

    Updated to match XLS. Key changes... Behavior of atan2 is different to XLS so swapped x & y (caused the 90 degree rotation of translation) GetCurrentHeading depends on where the the control hub is located (top side or bottom side)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoHolodrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoHolodrive.java
@@ -53,7 +53,8 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
+//@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
+@TeleOp(name="Auto Holo", group="Linear Opmode")
 @Disabled
 public class AutoHolodrive extends LinearOpMode {
 
